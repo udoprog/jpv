07e595122ef858738cba31a734dda07311445fe8
John-John Tedro <udoprog@tedro.se>
2023-10-08 17:25:36 +0200

    Update project

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,259 @@
+use std::borrow::Cow;
+use std::collections::{BTreeSet, HashSet};
+use std::fmt;
+use std::io::Write;
+use std::mem;
+use std::path::Path;
+
+use anyhow::{anyhow, Context, Result};
+use clap::Parser;
+use lib::adjective;
+use lib::database::{Database, IndexSource};
+use lib::verb;
+use lib::{Form, Furigana, PartOfSpeech};
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+#[derive(Parser)]
+struct Args {
+    /// Filter by parts of speech. If no arguments are specified, will filter by
+    /// entries which matches all specified parts of speech.
+    #[arg(long = "pos", name = "pos")]
+    parts_of_speech: Vec<String>,
+    /// List available parts of speech options an exit.
+    #[arg(long)]
+    list_pos: bool,
+    /// Perform inflection.
+    #[arg(long)]
+    inflection: bool,
+    /// Show examples for results.
+    #[arg(long)]
+    examples: bool,
+    /// Show glossary entries for the specified language. Defaults to "eng".
+    #[arg(long)]
+    lang: Option<String>,
+    /// Show glossary entries for any language. Overrides `--lang <lang>`.
+    #[arg(long)]
+    any_lang: bool,
+    /// Don't print output in furigana.
+    #[arg(long)]
+    no_furigana: bool,
+    /// Search arguments to filter by. Must be either kana or kanji, which is
+    /// matched against entries searched for.
+    #[arg(name = "arguments")]
+    arguments: Vec<String>,
+    /// Include polite variants of inflections.
+    #[arg(long)]
+    polite: bool,
+    /// Only fetch the specified sequence ids.
+    #[arg(long = "seq")]
+    sequences: Vec<u64>,
+}
+
+#[cfg(not(feature = "embed"))]
+#[inline]
+fn load_database(path: &Path) -> Result<Cow<'static, [u8]>> {
+    Ok(Cow::Owned(std::fs::read(path)?))
+}
+
+#[cfg(feature = "embed")]
+#[inline]
+fn load_database(_: &Path) -> Result<Cow<'static, [u8]>> {
+    const BYTES: &[u8] = include_bytes!("../../../database.bin");
+    Ok(Cow::Borrowed(BYTES))
+}
+
+fn main() -> Result<()> {
+    let filter = EnvFilter::builder().from_env_lossy();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .finish()
+        .try_init()?;
+
+    let database_path = Path::new("database.bin");
+
+    let args = Args::try_parse()?;
+
+    if args.list_pos {
+        println!("Available `--pos` arguments:");
+
+        for pos in PartOfSpeech::VALUES {
+            println!("{} - {} / {}", pos.ident(), pos.variant(), pos.help());
+        }
+
+        return Ok(());
+    }
+
+    let data =
+        load_database(database_path).with_context(|| anyhow!("{}", database_path.display()))?;
+
+    let db = Database::new(data.as_ref())?;
+
+    let mut to_look_up = BTreeSet::new();
+
+    for &seq in &args.sequences {
+        to_look_up.extend(db.lookup_sequence(seq));
+    }
+
+    for input in &args.arguments {
+        let seed = args.sequences.is_empty();
+
+        if seed {
+            to_look_up.extend(db.lookup(input));
+        } else {
+            let filter = db
+                .lookup(input)
+                .map(|id| id.index())
+                .collect::<HashSet<_>>();
+            to_look_up.retain(|id| filter.contains(&id.index()));
+        }
+    }
+
+    if !args.parts_of_speech.is_empty() {
+        let mut seed = args.arguments.is_empty() && args.sequences.is_empty();
+
+        for pos in &args.parts_of_speech {
+            let pos = PartOfSpeech::parse_keyword(pos)
+                .with_context(|| anyhow!("Invalid part of speech `{pos}`"))?;
+
+            let indexes = db.by_pos(pos);
+
+            if mem::take(&mut seed) {
+                to_look_up.extend(indexes);
+                continue;
+            }
+
+            to_look_up.retain(|index| indexes.contains(index));
+        }
+    }
+
+    let current_lang = args.lang.as_deref().unwrap_or("eng");
+
+    for (i, index) in to_look_up.iter().enumerate() {
+        let extra = match index.source() {
+            IndexSource::VerbInflection { inflection, .. } => {
+                Some(format!("Found through verb inflection: {inflection:?}"))
+            }
+            IndexSource::AdjectiveInflection { inflection } => Some(format!(
+                "Found through adjective inflection: {inflection:?}"
+            )),
+            _ => None,
+        };
+
+        let d = db.get(*index)?;
+
+        if let Some(extra) = extra {
+            println!("{extra}");
+        }
+
+        println!("#{i} Sequence: {}", d.sequence);
+
+        for (index, reading) in d.reading_elements.iter().enumerate() {
+            println!("  #{index} {:?}", reading.debug_sparse());
+        }
+
+        for (index, kanji) in d.kanji_elements.iter().enumerate() {
+            println!("  #{index} {:?}", kanji.debug_sparse());
+        }
+
+        for (index, sense) in d.senses.iter().enumerate() {
+            if !args.any_lang && !sense.is_lang(current_lang) {
+                continue;
+            }
+
+            println!("  #{index} {:?}", sense.debug_sparse());
+
+            for g in &sense.gloss {
+                if let Some(lang) = g.lang {
+                    println!("  - {} ({lang})", g.text);
+                } else {
+                    println!("  - {}", g.text);
+                }
+            }
+
+            if args.examples && !sense.examples.is_empty() {
+                println!("  Examples:");
+
+                for e in &sense.examples {
+                    println!("  - {e:?}");
+                }
+            }
+        }
+
+        if !args.inflection || (to_look_up.len() > 1 && args.sequences.is_empty()) {
+            continue;
+        }
+
+        let p = "  ";
+
+        let dis0 = |furigana| maybe_furigana(furigana, !args.no_furigana);
+        let dis = |furigana| maybe_furigana(furigana, !args.no_furigana);
+
+        let stdout = std::io::stdout();
+        let mut o = stdout.lock();
+
+        if let Some(c) = verb::conjugate(&d) {
+            writeln!(o, "{p}# Inflections:")?;
+
+            writeln!(o, "{p}  Dictionary:")?;
+            writeln!(o, "{p}  - {}", dis0(c.dictionary.furigana()))?;
+
+            for (c, form) in c.inflections {
+                if args.polite != c.contains(Form::Polite) {
+                    continue;
+                }
+
+                writeln!(o, "{p}  {c:?}:")?;
+                writeln!(o, "{p}  - {}", dis(form.furigana()))?;
+            }
+        }
+
+        if let Some(c) = adjective::conjugate(&d) {
+            writeln!(o, "{p}# Inflections:")?;
+
+            writeln!(o, "{p}  Dictionary:")?;
+            writeln!(o, "{p}  - {}", dis0(c.dictionary.furigana()))?;
+
+            for (c, form) in c.inflections {
+                if args.polite != c.contains(Form::Polite) {
+                    continue;
+                }
+
+                writeln!(o, "{p}  {c:?}:")?;
+                writeln!(o, "{p}  - {}", dis(form.furigana()))?;
+            }
+        }
+
+        o.flush()?;
+    }
+
+    Ok(())
+}
+
+fn maybe_furigana<const N: usize, const S: usize>(
+    furigana: Furigana<'_, N, S>,
+    do_furigana: bool,
+) -> impl fmt::Display + '_ {
+    struct Display<'a, const N: usize, const S: usize> {
+        furigana: Furigana<'a, N, S>,
+        do_furigana: bool,
+    }
+
+    impl<const N: usize, const S: usize> fmt::Display for Display<'_, N, S> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            if self.do_furigana {
+                self.furigana.fmt(f)
+            } else if self.furigana.kanji() != self.furigana.reading() {
+                write!(f, "{} ({})", self.furigana.kanji(), self.furigana.reading())
+            } else {
+                write!(f, "{}", self.furigana.kanji())
+            }
+        }
+    }
+
+    Display {
+        furigana,
+        do_furigana,
+    }
+}
--- crates/jpv/src/main.rs
+++ crates/jpv/src/main.rs
@@ -1,69 +1,32 @@
-use std::borrow::Cow;
-use std::collections::{BTreeSet, HashSet};
-use std::fmt;
-use std::io::Write;
-use std::mem;
-use std::path::Path;
+#![cfg_attr(all(not(feature = "cli"), windows), windows_subsystem = "windows")]
 
-use anyhow::{anyhow, Context, Result};
+use std::net::SocketAddr;
+
+use anyhow::{Context, Error, Result};
+use axum::body::{boxed, Body};
+use axum::extract::Query;
+use axum::http::{HeaderValue, Method, StatusCode};
+use axum::response::{IntoResponse, Response};
+use axum::{Extension, Json};
 use clap::Parser;
-use lib::adjective;
-use lib::database::{Database, IndexSource};
-use lib::verb;
-use lib::{Form, Furigana, PartOfSpeech};
+use lib::database::{Database, EntryResultKey};
+use lib::elements::{Entry, EntryKey};
+use serde::{Deserialize, Serialize};
+use tokio::signal::ctrl_c;
+use tokio::signal::windows::ctrl_shutdown;
+use tower_http::cors::CorsLayer;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 
 #[derive(Parser)]
 struct Args {
-    /// Filter by parts of speech. If no arguments are specified, will filter by
-    /// entries which matches all specified parts of speech.
-    #[arg(long = "pos", name = "pos")]
-    parts_of_speech: Vec<String>,
-    /// List available parts of speech options an exit.
+    /// Bind to the given address. Default is `127.0.0.1:8081`.
     #[arg(long)]
-    list_pos: bool,
-    /// Perform inflection.
-    #[arg(long)]
-    inflection: bool,
-    /// Show examples for results.
-    #[arg(long)]
-    examples: bool,
-    /// Show glossary entries for the specified language. Defaults to "eng".
-    #[arg(long)]
-    lang: Option<String>,
-    /// Show glossary entries for any language. Overrides `--lang <lang>`.
-    #[arg(long)]
-    any_lang: bool,
-    /// Don't print output in furigana.
-    #[arg(long)]
-    no_furigana: bool,
-    /// Search arguments to filter by. Must be either kana or kanji, which is
-    /// matched against entries searched for.
-    #[arg(name = "arguments")]
-    arguments: Vec<String>,
-    /// Include polite variants of inflections.
-    #[arg(long)]
-    polite: bool,
-    /// Only fetch the specified sequence ids.
-    #[arg(long = "seq")]
-    sequences: Vec<u64>,
+    bind: Option<String>,
 }
 
-#[cfg(not(feature = "embed"))]
-#[inline]
-fn load_database(path: &Path) -> Result<Cow<'static, [u8]>> {
-    Ok(Cow::Owned(std::fs::read(path)?))
-}
-
-#[cfg(feature = "embed")]
-#[inline]
-fn load_database(_: &Path) -> Result<Cow<'static, [u8]>> {
-    const BYTES: &[u8] = include_bytes!("../../../database.bin");
-    Ok(Cow::Borrowed(BYTES))
-}
-
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let filter = EnvFilter::builder().from_env_lossy();
 
     tracing_subscriber::fmt()
@@ -71,189 +34,226 @@ fn main() -> Result<()> {
         .finish()
         .try_init()?;
 
-    let database_path = Path::new("database.bin");
-
     let args = Args::try_parse()?;
+    let bind: SocketAddr = args.bind.as_deref().unwrap_or(self::bundle::BIND).parse()?;
 
-    if args.list_pos {
-        println!("Available `--pos` arguments:");
+    // SAFETY: we know this is only initialized once here exclusively.
+    let data = unsafe { self::bundle::database()? };
 
-        for pos in PartOfSpeech::VALUES {
-            println!("{} - {} / {}", pos.ident(), pos.variant(), pos.help());
+    tracing::info!("Loading database...");
+    let db = lib::database::Database::new(data).context("loading database")?;
+    tracing::info!("Database loaded");
+
+    let cors = CorsLayer::new()
+        .allow_origin(format!("http://localhost:8080").parse::<HeaderValue>().unwrap())
+        .allow_origin(format!("http://127.0.0.1:8080").parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET]);
+
+    let app = self::bundle::router().layer(Extension(db)).layer(cors);
+
+    tracing::info!("Listening on {bind}");
+
+    let server = axum::Server::bind(&bind).serve(app.into_make_service());
+    self::bundle::open();
+
+    let ctrl_c = ctrl_c();
+    let mut shutdown = ctrl_shutdown()?;
+
+    tokio::select! {
+        result = server => {
+            result?;
         }
-
-        return Ok(());
-    }
-
-    let data =
-        load_database(database_path).with_context(|| anyhow!("{}", database_path.display()))?;
-
-    let db = Database::new(data.as_ref())?;
-
-    let mut to_look_up = BTreeSet::new();
-
-    for &seq in &args.sequences {
-        to_look_up.extend(db.lookup_sequence(seq));
-    }
-
-    for input in &args.arguments {
-        let seed = args.sequences.is_empty();
-
-        if seed {
-            to_look_up.extend(db.lookup(input));
-        } else {
-            let filter = db
-                .lookup(input)
-                .map(|id| id.index())
-                .collect::<HashSet<_>>();
-            to_look_up.retain(|id| filter.contains(&id.index()));
+        _ = ctrl_c => {
+            tracing::info!("Shutting down...");
         }
-    }
-
-    if !args.parts_of_speech.is_empty() {
-        let mut seed = args.arguments.is_empty() && args.sequences.is_empty();
-
-        for pos in &args.parts_of_speech {
-            let pos = PartOfSpeech::parse_keyword(pos)
-                .with_context(|| anyhow!("Invalid part of speech `{pos}`"))?;
-
-            let indexes = db.by_pos(pos);
-
-            if mem::take(&mut seed) {
-                to_look_up.extend(indexes);
-                continue;
-            }
-
-            to_look_up.retain(|index| indexes.contains(index));
+        _ = shutdown.recv() => {
+            tracing::info!("Shutting down...");
         }
-    }
-
-    let current_lang = args.lang.as_deref().unwrap_or("eng");
-
-    for (i, index) in to_look_up.iter().enumerate() {
-        let extra = match index.source() {
-            IndexSource::VerbInflection { inflection } => {
-                Some(format!("Found through verb inflection: {inflection:?}"))
-            }
-            IndexSource::AdjectiveInflection { inflection } => Some(format!(
-                "Found through adjective inflection: {inflection:?}"
-            )),
-            _ => None,
-        };
-
-        let d = db.get(*index)?;
-
-        if let Some(extra) = extra {
-            println!("{extra}");
-        }
-
-        println!("#{i} Sequence: {}", d.sequence);
-
-        for (index, reading) in d.reading_elements.iter().enumerate() {
-            println!("  #{index} {:?}", reading.debug_sparse());
-        }
-
-        for (index, kanji) in d.kanji_elements.iter().enumerate() {
-            println!("  #{index} {:?}", kanji.debug_sparse());
-        }
-
-        for (index, sense) in d.senses.iter().enumerate() {
-            if !args.any_lang && !sense.is_lang(current_lang) {
-                continue;
-            }
-
-            println!("  #{index} {:?}", sense.debug_sparse());
-
-            for g in &sense.gloss {
-                if let Some(lang) = g.lang {
-                    println!("  - {} ({lang})", g.text);
-                } else {
-                    println!("  - {}", g.text);
-                }
-            }
-
-            if args.examples && !sense.examples.is_empty() {
-                println!("  Examples:");
-
-                for e in &sense.examples {
-                    println!("  - {e:?}");
-                }
-            }
-        }
-
-        if !args.inflection || (to_look_up.len() > 1 && args.sequences.is_empty()) {
-            continue;
-        }
-
-        let p = "  ";
-
-        let dis0 = |furigana| maybe_furigana(furigana, !args.no_furigana);
-        let dis = |furigana| maybe_furigana(furigana, !args.no_furigana);
-
-        let stdout = std::io::stdout();
-        let mut o = stdout.lock();
-
-        if let Some(c) = verb::conjugate(&d) {
-            writeln!(o, "{p}# Inflections:")?;
-
-            writeln!(o, "{p}  Dictionary:")?;
-            writeln!(o, "{p}  - {}", dis0(c.dictionary.furigana()))?;
-
-            for (c, form) in c.inflections {
-                if args.polite != c.contains(Form::Polite) {
-                    continue;
-                }
-
-                writeln!(o, "{p}  {c:?}:")?;
-                writeln!(o, "{p}  - {}", dis(form.furigana()))?;
-            }
-        }
-
-        if let Some(c) = adjective::conjugate(&d) {
-            writeln!(o, "{p}# Inflections:")?;
-
-            writeln!(o, "{p}  Dictionary:")?;
-            writeln!(o, "{p}  - {}", dis0(c.dictionary.furigana()))?;
-
-            for (c, form) in c.inflections {
-                if args.polite != c.contains(Form::Polite) {
-                    continue;
-                }
-
-                writeln!(o, "{p}  {c:?}:")?;
-                writeln!(o, "{p}  - {}", dis(form.furigana()))?;
-            }
-        }
-
-        o.flush()?;
     }
 
     Ok(())
 }
 
-fn maybe_furigana<const N: usize, const S: usize>(
-    furigana: Furigana<'_, N, S>,
-    do_furigana: bool,
-) -> impl fmt::Display + '_ {
-    struct Display<'a, const N: usize, const S: usize> {
-        furigana: Furigana<'a, N, S>,
-        do_furigana: bool,
+type RequestResult<T> = std::result::Result<T, RequestError>;
+
+struct RequestError {
+    error: anyhow::Error,
+}
+
+impl From<anyhow::Error> for RequestError {
+    #[inline]
+    fn from(error: anyhow::Error) -> Self {
+        Self { error }
+    }
+}
+
+#[derive(Deserialize)]
+struct SearchRequest {
+    q: Option<String>,
+}
+
+#[derive(Serialize)]
+struct SearchEntry {
+    key: EntryResultKey,
+    entry: Entry<'static>,
+}
+
+#[derive(Serialize)]
+struct SearchResponse {
+    entries: Vec<SearchEntry>,
+}
+
+async fn search(
+    Query(request): Query<SearchRequest>,
+    Extension(db): Extension<Database<'static>>,
+) -> RequestResult<Json<SearchResponse>> {
+    let Some(q) = request.q.as_deref() else {
+        return Err(Error::msg("Missing `q`").into());
+    };
+
+    let mut entries = Vec::new();
+
+    for (key, entry) in db.search(q)? {
+        entries.push(SearchEntry { key, entry });
     }
 
-    impl<const N: usize, const S: usize> fmt::Display for Display<'_, N, S> {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            if self.do_furigana {
-                self.furigana.fmt(f)
-            } else if self.furigana.kanji() != self.furigana.reading() {
-                write!(f, "{} ({})", self.furigana.kanji(), self.furigana.reading())
-            } else {
-                write!(f, "{}", self.furigana.kanji())
+    entries.sort_by(|a, b| a.key.key.cmp(&b.key.key));
+    Ok(Json(SearchResponse { entries }))
+}
+
+#[derive(Deserialize)]
+struct AnalyzeRequest {
+    q: String,
+    start: usize,
+}
+
+#[derive(Serialize)]
+struct AnalyzeEntry {
+    key: EntryKey,
+    string: String,
+}
+
+#[derive(Serialize)]
+struct AnalyzeResponse {
+    data: Vec<AnalyzeEntry>,
+}
+
+async fn analyze(
+    Query(request): Query<AnalyzeRequest>,
+    Extension(db): Extension<Database<'static>>,
+) -> RequestResult<Json<AnalyzeResponse>> {
+    let mut entries = Vec::new();
+
+    for (key, string) in db.analyze(&request.q, request.start) {
+        entries.push(AnalyzeEntry { key, string });
+    }
+
+    Ok(Json(AnalyzeResponse { data: entries }))
+}
+
+impl IntoResponse for RequestError {
+    fn into_response(self) -> Response {
+        tracing::error!("{}", self.error);
+        let mut response = Response::new(boxed(Body::empty()));
+        *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+        response
+    }
+}
+
+#[cfg(not(feature = "bundle"))]
+mod bundle {
+    use std::path::PathBuf;
+
+    use anyhow::{Result, Context};
+    use axum::Router;
+    use axum::routing::get;
+
+    pub(super) static BIND: &'static str = "127.0.0.1:8081";
+
+    static mut DATABASE: Vec<u8> = Vec::new();
+
+    pub(super) unsafe fn database() -> Result<&'static [u8]> {
+        let root = PathBuf::from(
+            std::env::var_os("CARGO_MANIFEST_DIR").context("missing CARGO_MANIFEST_DIR")?,
+        );
+
+        let path = root.join("..").join("..").join("database.bin");
+
+        tracing::info!("Reading from {}", path.display());
+
+        let data =
+            std::fs::read(&path).with_context(|| path.display().to_string())?;
+
+        DATABASE = data;
+        Ok(DATABASE.as_ref())
+    }
+
+    pub(super) fn open() {
+    }
+
+    pub(super) fn router() -> Router {
+        Router::new()
+            .route("/analyze", get(super::analyze))
+            .route("/search", get(super::search))
+    }
+}
+
+#[cfg(feature = "bundle")]
+mod bundle {
+    use std::borrow::Cow;
+
+    use anyhow::Result;
+    use axum::Router;
+    use axum::http::{header, StatusCode, Uri};
+    use axum::response::{IntoResponse, Response};
+    use axum::routing::get;
+    use rust_embed::RustEmbed;
+
+    pub(super) static BIND: &'static str = "127.0.0.1:8080";
+
+    static DATABASE: &'static [u8] =
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../database.bin"));
+
+    pub(super) unsafe fn database() -> Result<&'static [u8]> {
+        Ok(&DATABASE)
+    }
+
+    pub(super) fn open() {
+        let _ = webbrowser::open("http://localhost:8080");
+    }
+
+    pub(super) fn router() -> Router {
+        Router::new()
+            .route("/", get(index_handler))
+            .route("/api/analyze", get(super::analyze))
+            .route("/api/search", get(super::search))
+            .route("/*file", get(static_handler))
+            .fallback(index_handler)
+    }
+
+    async fn index_handler() -> impl IntoResponse {
+        StaticFile(Cow::Borrowed("index.html"))
+    }
+
+    async fn static_handler(uri: Uri) -> impl IntoResponse {
+        StaticFile(Cow::Owned(uri.path().trim_start_matches('/').to_string()))
+    }
+
+    #[derive(RustEmbed)]
+    #[folder = "../web/dist"]
+    struct Asset;
+
+    pub struct StaticFile(Cow<'static, str>);
+
+    impl IntoResponse for StaticFile {
+        fn into_response(self) -> Response {
+            match Asset::get(self.0.as_ref()) {
+                Some(content) => {
+                    let mime = mime_guess::from_path(self.0.as_ref()).first_or_octet_stream();
+                    ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+                }
+                None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
             }
         }
-    }
-
-    Display {
-        furigana,
-        do_furigana,
     }
 }
