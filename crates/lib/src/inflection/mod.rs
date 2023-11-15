4dbb35e6cf24ea4869f0b1f35d92213b7a44240e
John-John Tedro <udoprog@tedro.se>
2023-11-15 16:37:51 +0100

    More tutorial links

--- crates/lib/src/inflection/mod.rs
+++ crates/lib/src/inflection/mod.rs
@@ -19,13 +19,13 @@ use serde::{Deserialize, Serialize};
 use crate::kana::{Fragments, Full, OwnedFull};
 
 macro_rules! form {
-    ($vis:vis enum $name:ident { $({$variant:ident = $d:literal, $describe:literal, $title:literal, $url:expr $(,)?}),* $(,)? }) => {
+    ($vis:vis enum $name:ident { $({$variant:ident $(= $d:literal)?, $describe:literal, $title:literal, $url:expr $(,)?}),* $(,)? }) => {
         #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
         #[derive(Encode, Decode, Serialize, Deserialize, Key)]
         #[key(bitset = 8)]
         #[serde(rename_all = "kebab-case")]
         $vis enum $name {
-            $($variant = $d,)*
+            $($variant $(= $d)*,)*
         }
 
         impl $name {
@@ -59,37 +59,37 @@ macro_rules! form {
 
 form! {
     pub enum Form {
-        {Stem = 0, "stem", "stem / infinite", None},
-        {Honorific = 27, "敬語", "敬語 (ていご) honorific speech", None},
-        {Negative = 1, "not", "not doing ~, the absense of ~", None},
-        {Te = 17, "～て", "～te form, by itself acts as a command", None},
-        {TeAru = 18, "～てある", "～てある, resulting, is / has been done", None},
-        {TeIku = 19, "～ていく", "～ていく, starting, to start, to continue, to go on", None},
-        {TeIru = 20, "～ている", "～ている, progressive, shows that something is currently happening or ongoing", Some("https://www.tofugu.com/japanese-grammar/verb-continuous-form-teiru/")},
-        {TeKuru = 21, "～てくる", "～てくる, to do .. and come back, to become, to continue, to start ~", None},
-        {TeOku = 22, "～ておく", "～ておく, to do something in advance", None},
-        {TeShimau = 23, "～てしまう", "～てしまう, to do something by accident, to finish completely", None},
-        {Tai = 15, "～たい", "～たい, expressing desire", Some("https://www.tofugu.com/japanese-grammar/tai-form/")},
-        {EasyTo = 29, "easy", "～やすい, easy to do ~", Some("https://www.tofugu.com/japanese-grammar/yasui/")},
-        {HardTo = 30, "hard", "～にくい, hard to do ~", Some("https://www.tofugu.com/japanese-grammar/nikui/")},
-        {TaGaRu = 16, "～たがる", "～たがる, noting desire", Some("https://www.tofugu.com/japanese-grammar/tagaru-form/")},
-        {Causative = 2, "caus", "causative, make ~ do something, let / allow ~", None},
-        {Chau = 3, "～ちゃう", "～ちゃう, to do something by accident, to finish completely", None},
-        {Command = 4, "cmd", "command forms, よ / なさい / ください", Some("https://www.tofugu.com/japanese-grammar/verb-command-form-ro/")},
-        {CommandTeKudasai = 5, "～てください", "～てください, alternate command form", None},
-        {CommandYo = 6, "～よ", "～よ, alternate command form", None},
-        {Conditional = 7, "cond", "～たら, conditional, if ~, when ~", Some("https://www.tofugu.com/japanese-grammar/conditional-form-tara/")},
-        {Darou = 9, "～だろう", "～だろう, alternate form", None},
-        {Hypothetical = 10, "hyp", "hypothetical, if ~", None},
-        {Kya = 11, "～きゃ", "～きゃ, alternative hypothetical negative, if not ~", None},
-        {Passive = 12, "psv", "～られる, passive, ~ was done to someone or something", Some("https://www.tofugu.com/japanese-grammar/verb-passive-form-rareru/")},
-        {Past = 13, "past", "過去形 (かこけい) past tense", None},
-        {Potential = 14, "pot", "potential, can do ~", None},
-        {Simultaneous = 28, "～ながら", "～ながら, simultaneous, while ~", Some("https://www.tofugu.com/japanese-grammar/verb-nagara/")},
-        {Volitional = 24, "vol", "～よう, volitional / presumptive, let's do ~", Some("https://www.tofugu.com/japanese-grammar/verb-volitional-form-you/")},
-        {LooksLike = 26, "～そう", "～そう, looks like", Some("https://www.tofugu.com/japanese-grammar/verb-sou/")},
-        {Short = 25, "short", "alternate shortened form", None},
-        {Conversation = 8, "clq", "conversational / colloquial", None},
+        {Stem, "stem", "stem / infinite", None},
+        {Honorific, "敬語", "敬語 (ていご) honorific speech", None},
+        {Negative, "not", "not doing ~, the absense of ~", None},
+        {Te, "～て", "～te form, by itself acts as a command", Some("https://www.tofugu.com/japanese-grammar/te-form/")},
+        {TeAru, "～てある", "～てある, resulting, is / has been done", Some("https://www.tofugu.com/japanese-grammar/tearu/")},
+        {TeIru, "～ている", "～ている, progressive, shows that something is currently happening or ongoing", Some("https://www.tofugu.com/japanese-grammar/verb-continuous-form-teiru/")},
+        {TeIku, "～ていく", "～ていく, starting, to start, to continue, to go on", Some("https://www.tofugu.com/japanese-grammar/teiku-tekuru/")},
+        {TeKuru, "～てくる", "～てくる, to do .. and come back, to become, to continue, to start ~", Some("https://www.tofugu.com/japanese-grammar/teiku-tekuru/")},
+        {TeOku, "～ておく", "～ておく, to do something in advance", Some("https://www.tofugu.com/japanese-grammar/teoku/")},
+        {TeShimau, "～てしまう", "～てしまう, to do something by accident, to finish completely", None},
+        {Tai, "～たい", "～たい, expressing desire", Some("https://www.tofugu.com/japanese-grammar/tai-form/")},
+        {EasyTo, "easy", "～やすい, easy to do ~", Some("https://www.tofugu.com/japanese-grammar/yasui/")},
+        {HardTo, "hard", "～にくい, hard to do ~", Some("https://www.tofugu.com/japanese-grammar/nikui/")},
+        {TaGaRu, "～たがる", "～たがる, noting desire", Some("https://www.tofugu.com/japanese-grammar/tagaru-form/")},
+        {Causative, "caus", "causative, make ~ do something, let / allow ~", Some("https://www.tofugu.com/japanese-grammar/verb-causative-form-saseru/")},
+        {Chau, "～ちゃう", "～ちゃう, to do something by accident, to finish completely", None},
+        {Command, "cmd", "command forms, よ / なさい / ください", Some("https://www.tofugu.com/japanese-grammar/verb-command-form-ro/")},
+        {CommandTeKudasai, "～てください", "～てください, alternate command form", Some("https://www.tofugu.com/japanese-grammar/kudasai/")},
+        {CommandYo, "～よ", "～よ, alternate command form", None},
+        {Conditional, "cond", "～たら, conditional, if ~, when ~", Some("https://www.tofugu.com/japanese-grammar/conditional-form-tara/")},
+        {Darou, "～だろう", "～だろう, alternate form", Some("https://www.tofugu.com/japanese-grammar/darou/")},
+        {Hypothetical, "hyp", "hypothetical, if ~", None},
+        {Kya, "～きゃ", "～きゃ, alternative hypothetical negative, if not ~", None},
+        {Passive, "psv", "～られる, passive, ~ was done to someone or something", Some("https://www.tofugu.com/japanese-grammar/verb-passive-form-rareru/")},
+        {Past, "past", "過去形 (かこけい) past tense", None},
+        {Potential, "pot", "potential, can do ~", Some("https://www.tofugu.com/japanese-grammar/verb-potential-form-reru/")},
+        {Simultaneous, "～ながら", "～ながら, simultaneous, while ~", Some("https://www.tofugu.com/japanese-grammar/verb-nagara/")},
+        {Volitional, "vol", "～よう, volitional / presumptive, let's do ~", Some("https://www.tofugu.com/japanese-grammar/verb-volitional-form-you/")},
+        {LooksLike, "～そう", "～そう, looks like", Some("https://www.tofugu.com/japanese-grammar/verb-sou/")},
+        {Short, "short", "alternate shortened form", None},
+        {Conversation, "clq", "conversational / colloquial", None},
     }
 }
 
