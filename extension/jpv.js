35031ea84e52e6eb69a71a6db94ab35286b6c0cf
John-John Tedro <udoprog@tedro.se>
2023-11-30 12:23:02 +0100

    Update extension

--- extension/jpv.js
+++ extension/jpv.js
@@ -2,13 +2,13 @@ const DEBUG = false;
 const WIDTH = 400;
 const HEIGHT = 600;
 const PADDING = 10;
-const SELECT = false;
+const SELECT = true;
 const FOLLOWMOUSE = false;
 const MAX_X_OFFSET = 1024;
 
 let iframe = null;
 let loadListener = null;
-let currentPhrase = null;
+let currentText = null;
 
 function isValidStart(el) {
     return el.localName !== "body";
@@ -19,7 +19,10 @@ function isInlineElement(el) {
     return style.display === "inline" || style.display === "inline-block";
 }
 
-function getPhrase(el) {
+/**
+ * @returns {Element | null} The bounding element or null if it contains no text.
+ */
+function getBoundingElement(el) {
     if (!el.textContent) {
         return null;
     }
@@ -40,17 +43,7 @@ function getPhrase(el) {
         }
     }
 
-    if (SELECT) {
-        let s = window.getSelection();
-
-        if (s.rangeCount > 0) {
-            let first = s.getRangeAt(0);
-            first.setStartBefore(current);
-            first.setEndAfter(current);
-        }
-    }
-
-    return { text: current.textContent.trim(), element: current };
+    return current;
 }
 
 function closeWindow() {
@@ -62,11 +55,148 @@ function closeWindow() {
     loadListener = null;
     iframe.classList.remove('active');
     iframe.src = '';
-    currentPhrase = null;
+    currentText = null;
     return true;
 }
 
-function windowPosition(element, e) {
+/**
+ * Narrows the specified range until it fits a natural word boundary.
+ *
+ * This is a fairly tricky operation to perform over a DOM, because it contains
+ * a bunch of mixed elements, and ranges operate over them.
+ *
+ * We start by narrowing the range from the right, we have to scan for the last
+ * referenced text node, check if it contains a natural paragraph boundary (the
+ * various forms of dots, exclamations, ...).
+ *
+ * Then we repeat the operation from the left.
+ *
+ * @param {Range} range The range to narrow, until it fits a natural text
+ * boundary which is pointed to by the cursor.
+ */
+function adjustRangeToBoundaries(range, x, y) {
+    let update = false;
+
+    update |= walk(range, x, y, {
+        direction: 'forward',
+        start: r => r.startContainer,
+        first: n => n.firstChild,
+        next: n => n.nextSibling,
+        iterate: bounds => bounds,
+        set: (r, n, i) => r.setStart(n, i),
+    });
+
+    update |= walk(range, x, y, {
+        direction: 'backward',
+        start: r => r.endContainer,
+        first: n => n.lastChild,
+        next: n => n.previousSibling,
+        iterate: bounds => bounds.reverse(),
+        set: (r, n, i) => r.setEnd(n, i),
+    });
+
+    return update;
+}
+
+/**
+ * @param {Range} original Original range to scan.
+ * @param {Factory}
+ * @returns {Range} The walked range range, or null if no valid range was found.
+ */
+function walk(original, x, y, factory) {
+    let range = original.cloneRange();
+    let node = factory.start(range);
+    let update = false;
+
+    outer:
+    while (node) {
+        if (node.nodeType != Node.TEXT_NODE) {
+            let first = factory.first(node);
+
+            if (first !== null) {
+                node = factory.first(node);
+                continue;
+            }
+
+            node = factory.next(node);
+            continue;
+        }
+
+        let boundaries = findBoundaries(node.textContent);
+
+        for (let i of factory.iterate(boundaries)) {
+            // Update the outer range.
+            factory.set(range, node, i + 1);
+
+            if (!rectContainsAny(range.getClientRects(), x, y)) {
+                break outer;
+            }
+
+            factory.set(original, node, i + 1);
+            update = true;
+        }
+
+        node = factory.next(node.parentNode);
+    }
+
+    return update;
+}
+
+function rectContainsAny(rects, x, y) {
+    for (let rect of rects) {
+        if (rectContains(rect, x, y)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function rectContains(rect, x, y) {
+    return rect.x <= x && rect.x + rect.width >= x && rect.y <= y && rect.y + rect.height >= y;
+}
+
+/**
+ * @param {string} content Content to scan.
+ * @returns {number[]} Boundaries found.
+ */
+function findBoundaries(content) {
+    let boundaries = [];
+    let whitespace = -1;
+    let leading = true;
+
+    for (let i = 0; i < content.length; i++) {
+        let c = content[i];
+
+        if (c === ' ' || c === '　' || c === '\n' || c === '\t') {
+            whitespace = i;
+            continue;
+        }
+
+        if (leading && i > 0) {
+            boundaries.push(i - 1);
+        }
+
+        leading = false;
+        whitespace = -1;
+
+        if (c === '.' || c === '。' || c === '!' || c === '！' || c === '?' || c === '？') {
+            boundaries.push(i);
+            leading = true;
+        }
+    }
+
+    if (whitespace !== -1) {
+        // NB: don't populate with only whitespace.
+        if (boundaries.length !== 0) {
+            boundaries.push(whitespace);
+        }
+    }
+
+    return boundaries;
+}
+
+function windowPosition(range, e) {
     let popupHeight = HEIGHT;
     let popupWidth = WIDTH;
     let padding = PADDING;
@@ -75,9 +205,6 @@ function windowPosition(element, e) {
     let windowHeight = window.innerHeight;
 
     if (!FOLLOWMOUSE) {
-        let range = document.createRange();
-        range.selectNodeContents(element);
-        range.detach();
         var rect = range.getBoundingClientRect();
 
         let maxX = e.clientX + MAX_X_OFFSET;
@@ -132,28 +259,48 @@ function windowPosition(element, e) {
 }
 
 function openWindow(e) {
-    let phrase = getPhrase(e.target);
+    let element = getBoundingElement(e.target);
 
-    if (phrase == null) {
+    if (element == null) {
         return;
     }
 
-    let { text, element } = phrase;
+    let elementRange = document.createRange();
+    elementRange.selectNodeContents(element);
+    let textRange = elementRange.cloneRange();
+    adjustRangeToBoundaries(textRange, e.clientX, e.clientY);
 
-    let pos = windowPosition(element, e);
+    let pos = windowPosition(elementRange, e);
+    let text = textRange.toString();
+
+    if (SELECT) {
+        let s = window.getSelection();
+
+        if (s.rangeCount > 0) {
+            let existing = s.getRangeAt(0);
+            existing.setStart(textRange.startContainer, textRange.startOffset);
+            existing.setEnd(textRange.endContainer, textRange.endOffset);
+
+            for (let i = 1; i < s.rangeCount; i++) {
+                s.removeRange(s.getRangeAt(i));
+            }
+        } else {
+            s.addRange(textRange);
+        }
+    }
 
     if (DEBUG) {
         console.debug(pos);
     }
 
-    if (currentPhrase != text) {
+    if (currentText != text) {
         if (!loadListener) {
             loadListener = () => iframe.classList.add('active');
             iframe.addEventListener('load', loadListener);
         }
 
         iframe.src = 'http://localhost:44714?embed=yes&q=' + encodeURIComponent(text);
-        currentPhrase = text;
+        currentText = text;
     }
 
     iframe.style.left = `${pos.x}px`;
